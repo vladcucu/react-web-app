867f3727555d9bc051b3ed283196818f93987581
vladcucu <55890763+vladcucu@users.noreply.github.com>
2021-06-06 11:57:26 -0400

    Add files via upload
    First version of the web app.
    Current functionalities: 
    -search function implemented (can repos by user name)
    -details of selected repo (forks, stars, language, name)

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { useEffect, useState} from "react";
+import axios from 'axios';
+import RepoDetails from "./RepoDetails";
+import './App.css';
+import { Alert } from "react-native";
+
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+  const [details, setDetails] = useState({});
+  const [detailsLoading, setDetailsLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState("Github Username");
+
+  useEffect(() => {
+    setRepos([]);
+    setDetails({});
+  }, [username]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    searchRepos();
+  };
+  
+  function searchRepos() {
+    setLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/repos`,
+    }).then(res => {
+      setLoading(false);
+      setRepos(res.data);
+    })
+    .catch(errro => {
+      setLoading(false);
+      setUsername("")
+      setPlaceholder("Wrong username, try again...")
+    });
+  }
+
+  function renderRepo(repo) {
+    return (
+      <div className="row" onClick={() => getDetails(repo.name)} key={repo.id}>
+        <h2 className="repo-name">
+          {repo.name}
+        </h2>
+      </div>
+    );
+  }
+
+  function getDetails(repoName) {
+    setDetailsLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/repos/${username}/${repoName}`,
+    }).then(res => {
+      setDetailsLoading(false);
+      setDetails(res.data)
+    });
+  }
+
+  return (
+    <div className="page">
+      <div className="landing-page-container">
+        <div className="left-side">
+          <form className="form">
+            <input className="input"
+            value={username}
+            placeholder={placeholder}
+            onChange={e => setUsername(e.target.value)}
+            />
+            <button className="button" onClick={handleSubmit}>{loading ? "Searching..." : "Search"}</button>
+          </form>
+          <div className="results-container">
+            {repos.map(renderRepo)}
+          </div>
+        </div>
+        <RepoDetails details={details} loading={detailsLoading} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
